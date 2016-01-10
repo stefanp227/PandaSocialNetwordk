93c262292ac46b78308da44261424ea3bd98db18
JustBePlayer <momcheto14@abv.bg>
2016-01-11 01:00:47 +0200

    save and load from file

--- Panda.rb
+++ Panda.rb
@@ -41,16 +41,111 @@ class Panda
 end
 
 class PandaSocialNetwork
+
+  require 'json'
+
+  attr_accessor :pandas 
+
   def initialize()
-    @pandas = []
+    @pandas = {}
   end
 
   def add_panda(panda)
-    @pandas << panda
+    raise "Panda Already There" if has_panda?(panda)
+    @pandas[panda] = []
+  end
+
+  def make_friends(panda1, panda2)
+    raise "Panda Is not There" unless has_panda?(panda1) and has_panda?(panda2)
+    @pandas[panda1] << panda2
+    @pandas[panda2] << panda1
   end
 
   def has_panda?(other)
-    return true if @pandas.any? {|panda| panda == other}
+    return true if @pandas.keys.any? {|panda| panda == other}
     false
   end
+
+  def are_friends?(panda1, panda2)
+    return true if @pandas[panda1].any? { |panda| panda == panda2 }
+    false    
+  end
+
+  def friends_of(panda)
+    return @pandas[panda] if has_panda?(panda)
+    false
+  end
+
+  def bfs(from, to)
+    queue = Queue.new
+    queue.push from
+    visited = [from]
+    #nodes, next_nodes = @panda[from], []
+
+    while !queue.empty?
+      el = queue.shift
+      nodes, next_nodes = @pandas[el], []
+      puts nodes
+      nodes.each do |node|
+        unless visited.any? { |e| e == node }
+          queue << node
+          visited << node
+        end
+      end
+   end
+
+   puts "Program end"
+  end
+
+  def connection_level(from, to)
+    return false unless has_panda?(from) and has_panda?(to)
+    queue, level, visited = Queue.new, 0, [from]
+    queue.push Hash[from, level]
+
+    while !queue.empty?
+      el = queue.shift
+      return el.values.first if el.keys.first == to
+
+      nodes = @pandas[el.keys.first]
+      nodes.each do |node|
+        unless visited.any? { |e| e == node }
+          queue << Hash[node,level+1]
+          visited << node
+        end
+      end
+      level += 1
+   end
+
+   -1
+  end
+
+  def get_nodes(panda)
+    [].tap do |arr|
+      panda.each do |node|
+        arr << node
+      end
+    end
+  end
+
+  def save(file)
+    File.open(file + ".json", "w") { |file| file.write(@pandas.to_json) }
+  end
+
+  def self.load(file)
+    
+    new_hash = {}
+
+    hash = JSON.parse(File.read(file + ".json"))
+    hash.each do |key, values|
+      val_pandas = []
+      values.each do |value|
+        val_pandas << Panda.new(*value.split(', '))
+      end
+      new_hash[Panda.new(*key.split(', '))] = val_pandas
+    end
+    PandaSocialNetwork.new.tap do |network|
+      network.pandas = new_hash
+    end
+  end
+
 end
